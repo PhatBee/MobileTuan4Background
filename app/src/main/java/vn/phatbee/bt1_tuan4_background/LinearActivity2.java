8c3dbabc14e96343252f0718b53ae775ac8be631
PhatBee <vinhphatst1235@gmail.com>
2025-02-21 23:00:55 +0700

    V3.1: SignupActivity

--- app/src/main/java/vn/phatbee/bt1_tuan4_background/LinearActivity2.java
+++ app/src/main/java/vn/phatbee/bt1_tuan4_background/LinearActivity2.java
@@ -15,7 +15,7 @@ public class LinearActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_linear2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.createText), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
