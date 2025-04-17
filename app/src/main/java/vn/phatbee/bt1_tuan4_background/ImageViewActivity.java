201c2ee26c8629cc191d141612bb8271590b969c
PhatBee <vinhphatst1235@gmail.com>
2025-04-18 04:42:12 +0700

    V4: ImageView

---
+++ app/src/main/java/vn/phatbee/bt1_tuan4_background/ImageViewActivity.java
@@ -0,0 +1,36 @@
+package vn.phatbee.bt1_tuan4_background;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ImageViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_image_view);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageView img1= (ImageView) findViewById(R.id.imageView1);
+        img1.setImageResource(R.drawable.kotlin);
+
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
