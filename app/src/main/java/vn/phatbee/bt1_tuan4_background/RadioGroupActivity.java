d499f644a8a8f68a82ecd5bef85e20c5bc580869
PhatBee <vinhphatst1235@gmail.com>
2025-04-18 05:05:55 +0700

    V8: RadioGroup

--- app/src/main/java/vn/phatbee/bt1_tuan4_background/RadioGroupActivity.java
+++ app/src/main/java/vn/phatbee/bt1_tuan4_background/RadioGroupActivity.java
@@ -1,10 +1,8 @@
 package vn.phatbee.bt1_tuan4_background;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,36 +11,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CheckBoxActivity extends AppCompatActivity {
+public class RadioGroupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_check_box);
+        setContentView(R.layout.activity_radio_group);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        CheckBox ck1 = (CheckBox) findViewById(R.id.checkBox);
+        //RadioGroup
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup1);
         ConstraintLayout bg = (ConstraintLayout) findViewById(R.id.main);
         Button btnNext = (Button) findViewById(R.id.btnNext);
-        ck1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        btnNext.setOnClickListener(v -> {
+            finish();
+        });
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.radioButton) {
                     bg.setBackgroundResource(R.drawable.background_intro);
-                } else {
+                } else if (checkedId == R.id.radioButton2) {
                     bg.setBackgroundResource(R.drawable.ic_launcher_background);
                 }
             }
-        });
-
-        btnNext.setOnClickListener(v -> {
-            Intent intent = new Intent(CheckBoxActivity.this, RadioGroupActivity.class);
-            startActivity(intent);
         });
     }
 }
